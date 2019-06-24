25a929a6a08a5d59a59d53b0781d4fddc99f5060
wilsontom <wilsontom89@gmail.com>
2019-06-24 17:05:14 +0100

    add rox comments

--- R/create_address_barcode.R
+++ R/create_address_barcode.R
@@ -1,9 +1,11 @@
 #' Create Address Barcode
 #'
-#' @param address
+#' This functions invokes a system command to `Zint` and outputs a **Code 128** barcode.
+#'
+#' Barcodes are saved to the directory which has been created using `create_directory`.
+#'
+#' @param address A character string with no white space which is used as the sample address
 #' @export
-#'
-#'
 
 create_address_barcode <- function(address = 'CarwynJamesBuilding')
 {
