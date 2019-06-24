922c87760cb22b05de0dfbb504486e934541e3b5
wilsontom <wilsontom89@gmail.com>
2019-06-24 17:05:32 +0100

    add rox comments

--- R/generate_barcodes.R
+++ R/generate_barcodes.R
@@ -1,9 +1,20 @@
+#' Generate Barcodes
 #'
+#' This is the main worker function of the whole package. `generate_barcodes` wraps up the barcode creation and arrangement and
+#' saves them in the specified directory.
 #'
+#' @param filePath A valid directory path where barcodes are saved
+#' @param ID A character string of participant ID
+#' @param DOB A character string of Date-of Birth in the following format; **DD/MM/YYYY**
+#' @param GENDER A character of either M or F (for Male and Female respectively)
+#' @param ADDRESS A character string with no white space which is used as the sample address
+#' @param TYPE A character vector of any of the following options;
+#'     - FORM
+#'     - LOG
+#'     - FBC
+#'     - CHEM21
 #'
 #' @export
-#'
-#'
 
 generate_barcodes <-
   function(filePath,
