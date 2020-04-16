87444f7e251bf3e1317acc0ecab9548e35c2c9a2
wilsontom <wilsontom89@gmail.com>
2020-04-16 12:05:34 +0100

    update with importing and file cleanup

--- R/stack_barcodes.R
+++ R/stack_barcodes.R
@@ -9,7 +9,6 @@
 #' @param TYPE A character vector of any of the following options; FORM, LOG, FBC, CHEM21
 #'
 #' @keywords internal
-#' @importFrom magick image_read image_scale image_annotate image_resize image_append image_write
 #' @importFrom magrittr %>%
 
 stack_barcodes <-
@@ -20,71 +19,81 @@ stack_barcodes <-
            TYPE)
   {
     id_barcode <-
-      image_read(paste0(getOption('savePath'), '/ID.png')) %>% image_resize(., '85%x100%')
+      magick::image_read(paste0(getOption('savePath'), '/ID.png'))
+
+    id_width <- image_width_resize(id_barcode)
+
+    id_barcode <- id_barcode %>% magick::image_resize(., id_width)
 
     dob_barcode <-
-      image_read(paste0(getOption('savePath'), '/DOB.png')) %>% image_resize(., '93%x100%')
+      magick::image_read(paste0(getOption('savePath'), '/DOB.png'))
 
     address_barcode <-
-      image_read(paste0(getOption('savePath'), '/ADDRESS.png')) %>% image_resize(., '51%x100%')
+      magick::image_read(paste0(getOption('savePath'), '/ADDRESS.png'))
+
+    address_width <- image_width_resize(address_barcode)
+
+    address_barcode <-
+      address_barcode %>% magick::image_resize(., address_width)
+
 
 
     white_space <-
-      image_read(system.file(package = 'bronglaisBarcodes', 'white_space.png'))
+      magick::image_read(system.file(package = 'bronglaisBarcodes', 'white_space.png'))
 
     white_space2 <-
-      image_read(system.file(package = 'bronglaisBarcodes', 'white_space.png')) %>% image_scale(., 'x5')
+      magick::image_read(system.file(package = 'bronglaisBarcodes', 'white_space.png')) %>% magick::image_scale(., 'x5')
 
 
     id_stack <-
-      c(id_barcode, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(.,
-                                                                                      ID,
-                                                                                      font = 'Arial',
-                                                                                      size = 14,
-                                                                                      location = '+80+30') %>%
-      image_annotate(.,
-                     GENDER,
-                     font = 'Arial',
-                     size = 14,
-                     location = '+210+30')
+      c(id_barcode, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(.,
+                                                                                                      ID,
+                                                                                                      font = 'Arial',
+                                                                                                      size = 14,
+                                                                                                      location = '+80+35') %>%
+      magick::image_annotate(.,
+                             GENDER,
+                             font = 'Arial',
+                             size = 14,
+                             location = '+210+35')
 
 
     dob_stack <-
-      c(dob_barcode, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(.,
-                                                                                       DOB,
-                                                                                       font = 'Arial',
-                                                                                       size = 14,
-                                                                                       location = '+80+35')
+      c(dob_barcode, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(.,
+                                                                                                       DOB,
+                                                                                                       font = 'Arial',
+                                                                                                       size = 14,
+                                                                                                       location = '+80+35')
 
     address_stack <-
-      c(address_barcode, white_space, white_space2) %>% image_append(., stack = TRUE) %>% image_annotate(.,
-                                                                                                         ADDRESS,
-                                                                                                         font = 'Arial',
-                                                                                                         size = 14,
-                                                                                                         location = '+50+32')
+      c(address_barcode, white_space, white_space2) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(.,
+                                                                                                                         ADDRESS,
+                                                                                                                         font = 'Arial',
+                                                                                                                         size = 14,
+                                                                                                                         location = '+50+32')
 
 
     if (TYPE == 'FORM') {
       type_stack <-
-        c(white_space, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(.,
-                                                                                         'FORM',
-                                                                                         font = 'Arial',
-                                                                                         size = 12,
-                                                                                         location = '+180+15')
+        c(white_space, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(.,
+                                                                                                         'FORM',
+                                                                                                         font = 'Arial',
+                                                                                                         size = 12,
+                                                                                                         location = '+180+15')
     }
 
     if (TYPE == 'LOG') {
       type_stack <-
-        c(white_space, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(.,
-                                                                                         'LOG',
-                                                                                         font = 'Arial',
-                                                                                         size = 12,
-                                                                                         location = '+180+15')
+        c(white_space, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(.,
+                                                                                                         'LOG',
+                                                                                                         font = 'Arial',
+                                                                                                         size = 12,
+                                                                                                         location = '+180+15')
     }
 
     if (TYPE == 'FBC') {
       type_stack <-
-        c(white_space, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(
+        c(white_space, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(
           .,
           'Purple top: FBC',
           font = 'Arial',
@@ -95,7 +104,7 @@ stack_barcodes <-
 
     if (TYPE == 'CHEM21') {
       type_stack <-
-        c(white_space, white_space) %>% image_append(., stack = TRUE) %>% image_annotate(
+        c(white_space, white_space) %>% magick::image_append(., stack = TRUE) %>% magick::image_annotate(
           .,
           'Yellow top: U&E,LFT,CRP,LIP,FER,AST,GGT',
           font = 'Arial',
@@ -105,10 +114,8 @@ stack_barcodes <-
     }
 
 
-
-
     final_barcode <-
-      c(white_space2, id_stack, address_stack, dob_stack, type_stack) %>% image_append(., stack = TRUE)
+      c(white_space2, id_stack, address_stack, dob_stack, type_stack) %>% magick::image_append(., stack = TRUE)
 
 
     return(final_barcode)
