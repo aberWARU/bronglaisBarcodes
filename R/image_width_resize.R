597e6231dfc39c7bcc9b0ae30442684bf0c1597c
wilsontom <wilsontom89@gmail.com>
2020-04-16 12:04:40 +0100

    add function for dynamic width resizing of barcodes relative to DOB barcode

---
+++ R/image_width_resize.R
@@ -0,0 +1,25 @@
+#' Resize Image Width
+#'
+#' Generate the values needed for `magick::image_resize()` to scale barcodes width relatve to the DOB barcode
+#'
+#' @param barcode_imaage a `magick-image` of barcode that has read in using `magick::image_read()`
+#' @return a character string of formated width and heght (ie, '93%x100%)
+#'
+#' @keywords internal
+
+image_width_resize <- function(barcode_image)
+{
+  image_attr_size <-
+    magick::image_attributes(barcode_image) %>% tibble::as_tibble() %>%
+    dplyr::filter(stringr::str_detect(property, '.width,height')) %>%
+    dplyr::select(value) %>% dplyr::pull() %>% stringr::str_split(., ',')
+
+  image_attr_width <- as.numeric(image_attr_size[[1]][[1]])
+
+  resize_value <- round((268 / image_attr_width) * 100, digits = 0)
+
+
+  format_value <- stringr::str_c(resize_value, '%', 'x100%')
+
+  return(format_value)
+}
